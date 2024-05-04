bf15d385bb4266f09ce6b0a98c55e8767caa9ea7
Grant <mpls@posteo.net>
2024-05-04 18:54:48 -0500

    make v1 connection
    Making this a v1 connection until we align our repo with most recent changes in core.
    
    Trying to avoid this error:
    AttributeError: 'Namespace' object has no attribute 'v2transport'

--- src/scenarios/replacement_cycling.py
+++ src/scenarios/replacement_cycling.py
@@ -230,7 +230,7 @@ class ReplacementCyclingTest(WarnetTestFramework):
         # sync_all() simply waits until blocks and mempools become synced
         time.sleep(4)
 
-        self.connect_nodes(0, 1)
+        self.connect_nodes(0, 1, peer_advertises_v2=False)
 
         coin_1 = self.wallet.get_utxo()
 
