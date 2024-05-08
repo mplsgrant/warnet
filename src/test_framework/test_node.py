579029e6476af06c7c3518bd054b8b28bedbccde
Grant <mpls@posteo.net>
2024-05-08 15:05:18 -0500

    add rpc logging

--- src/test_framework/test_node.py
+++ src/test_framework/test_node.py
@@ -809,6 +809,7 @@ class RPCOverloadWrapper():
         self.rpc = rpc
         self.is_cli = cli
         self.descriptors = descriptors
+        self.log = logging.getLogger("RPCOverloadWrapper")
 
     def __getattr__(self, name):
         return getattr(self.rpc, name)
@@ -817,6 +818,7 @@ class RPCOverloadWrapper():
         return self.__getattr__("createwallet")(*args, **kwargs)
 
     def createwallet(self, wallet_name, disable_private_keys=None, blank=None, passphrase='', avoid_reuse=None, descriptors=None, load_on_startup=None, external_signer=None):
+        self.log.info(f"create wallet {wallet_name}; {self.rpc.rpc_url}")
         if descriptors is None:
             descriptors = self.descriptors
         return self.__getattr__('createwallet')(wallet_name, disable_private_keys, blank, passphrase, avoid_reuse, descriptors, load_on_startup, external_signer)
