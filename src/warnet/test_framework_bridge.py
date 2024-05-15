4fd418cb43dd2b53b9bb91b7d19ed2c6103ee307
Grant <mpls@posteo.net>
2024-05-15 17:53:00 -0500

    go for the ip

--- src/warnet/test_framework_bridge.py
+++ src/warnet/test_framework_bridge.py
@@ -331,7 +331,7 @@ class WarnetTestFramework(BitcoinTestFramework):
         config.load_incluster_config()
         v1 = client.CoreV1Api()
         service = v1.read_namespaced_service(name="warnet-tank-000010-service", namespace="warnet")
-        ip = service.spec
+        ip = service.spec.cluster_ip
         to_connection.log.info(f"service ip: {ip}")
 
         for network_info in to_connection.getnetworkinfo()["localaddresses"]:
