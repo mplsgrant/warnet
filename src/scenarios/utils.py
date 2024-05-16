43c7dba7bde16b19ce73d42f190353041cd05ecf
Grant <mpls@posteo.net>
2024-05-15 21:08:57 -0500

    add endpoint logic to utils

--- src/scenarios/utils.py
+++ src/scenarios/utils.py
@@ -9,8 +9,12 @@ def ensure_miner(node):
     return node.get_wallet_rpc("miner")
 
 
-def get_service_ip(service_name: str, namespace: str = "warnet") -> IPv4Address | IPv6Address:
+def get_service_ip(service_name: str, namespace: str = "warnet") -> (IPv4Address | IPv6Address,
+                                                                     IPv4Address | IPv6Address):
+    """Given a service name and namespace, returns the services external ip and internal ip"""
     config.load_incluster_config()
     v1 = client.CoreV1Api()
     service = v1.read_namespaced_service(name=service_name, namespace=namespace)
-    return ip_address(service.spec.cluster_ip)
+    endpoints = v1.read_namespaced_endpoints(name="warnet-tank-000006-service", namespace="warnet")
+    inner_ip = endpoints.subsets[0].addresses[0].ip
+    return ip_address(service.spec.cluster_ip), ip_address(inner_ip)
