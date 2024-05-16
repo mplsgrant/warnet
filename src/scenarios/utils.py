062ee4ce1020e5de59de2d675acd916a6f58a9d3
Grant <mpls@posteo.net>
2024-05-15 20:30:16 -0500

    add get_service_ip util

--- src/scenarios/utils.py
+++ src/scenarios/utils.py
@@ -1,5 +1,16 @@
+from ipaddress import IPv4Address, IPv6Address, ip_address
+from kubernetes import client, config
+
+
 def ensure_miner(node):
     wallets = node.listwallets()
     if "miner" not in wallets:
         node.createwallet("miner", descriptors=True)
     return node.get_wallet_rpc("miner")
+
+
+def get_service_ip(service_name: str, namespace: str = "warnet") -> IPv4Address | IPv6Address:
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
+    service = v1.read_namespaced_service(name=service_name, namespace=namespace)
+    return ip_address(service.spec.cluster_ip)
