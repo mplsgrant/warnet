e04dbb37da3cb61fcfa11298e377fde5ac4932c7
Grant <mpls@posteo.net>
2024-05-23 19:11:52 -0500

    duplicate an existing scenario, renaming it

---
+++ src/scenarios/tx_flood2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from scenarios.utils import ensure_miner
+from warnet.test_framework_bridge import WarnetTestFramework
+
+BLOCKS = 100
+TXS = 100
+
+
+def cli_help():
+    return "Generate 100 blocks with 100 TXs each, again!"
+
+
+class TXFlood2(WarnetTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 1
+
+    def run_test(self):
+        miner = ensure_miner(self.nodes[0])
+        addr = miner.getnewaddress()
+        self.generatetoaddress(self.nodes[0], 200, addr)
+        for b in range(BLOCKS):
+            for t in range(TXS):
+                txid = self.nodes[0].sendtoaddress(address=addr, amount=0.001)
+                self.log.info(f"sending tx {t}/{TXS}: {txid}")
+            block = self.generate(self.nodes[0], 1)
+            self.log.info(f"generating block {b}/{BLOCKS}: {block}")
+
+
+if __name__ == "__main__":
+    TXFlood2().main()
