a6f587c685e2b690231dc54867a50489e05f6495
Grant <mpls@posteo.net>
2024-05-30 15:51:28 -0500

    add inspect_ip scenario to test subset[0] theory

---
+++ src/scenarios/inspect_ip.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from time import sleep
+
+from scenarios.utils import ensure_miner
+from warnet.test_framework_bridge import WarnetTestFramework
+
+from ipaddress import IPv4Address, IPv6Address, ip_address
+from kubernetes import client, config
+
+def cli_help():
+    return "Generate blocks over time. Options: [--allnodes | --interval=<number>]"
+
+
+class InspectIP(WarnetTestFramework):
+    def set_test_params(self):
+        # This is just a minimum
+        self.num_nodes = 0
+
+
+    def run_test(self):
+        config.load_incluster_config()
+        v1 = client.CoreV1Api()
+        service = v1.read_namespaced_service(name="warnet-tank-000000-service",
+                                             namespace="warnet")
+        endpoints = v1.read_namespaced_endpoints(name="warnet-tank-000000-service",
+                                                 namespace="warnet")
+        self.log.info(f"subsets: {endpoints.subsets}")
+        self.log.info(f"subsets[0].addresses: {endpoints.subsets[0].addresses}")
+        inner_ip = endpoints.subsets[0].addresses[0].ip  # Does our infra ensure 0th subset and address?
+        return ip_address(service.spec.cluster_ip), ip_address(inner_ip)
+
+
+if __name__ == "__main__":
+    InspectIP().main()
