63e9010f661b6045ee50bfb7cc4fe3cc4d11f017
Grant <mpls@posteo.net>
2024-06-20 16:32:26 -0500

    Beef up relay with logging and cleanup code

--- src/templates/msg_relay/relay.py
+++ src/templates/msg_relay/relay.py
@@ -1,14 +1,53 @@
 import zmq
+import signal
+import sys
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - Relay - %(message)s')
 
 context = zmq.Context()
 
-# Receiving socket
-rx = context.socket(zmq.SUB)
-rx.bind("tcp://*:5555")
-rx.setsockopt_string(zmq.SUBSCRIBE, "")
+frontend = context.socket(zmq.SUB)
+frontend.bind("tcp://*:5555")
+frontend.setsockopt_string(zmq.SUBSCRIBE, "")
 
-# Sending socket
-tx = context.socket(zmq.PUB)
-tx.bind("tcp://*:5556")
+backend = context.socket(zmq.PUB)
+backend.bind("tcp://*:5556")
 
-zmq.proxy(rx, tx)
+
+# Handle signals
+def signal_handler(sig, _frame):
+    """Handle shutdown signal events from the operating system."""
+    logging.info('Interrupt signal received: {} ({})'.format(sig, signal.Signals(sig).name))
+    logging.info('Stopping')
+    shutdown()
+    sys.exit(0)
+
+
+def shutdown():
+    """Cleanly shutdown the relay server."""
+    frontend.close()
+    backend.close()
+    context.term()
+
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+try:
+    logging.info("Starting")
+
+    while True:
+        try:
+            message = frontend.recv_string()
+            logging.info(f"Message: {message}")
+            backend.send_string(message)
+        except zmq.ZMQError as e:
+            logging.error(f"ZMQError: {e}")
+            break
+
+except Exception as e:
+    logging.error(f"Interrupted: {e}")
+finally:
+    shutdown()
