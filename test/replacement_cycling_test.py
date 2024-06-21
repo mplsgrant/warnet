67283fcb68d354f5c9941ac7660ae4dcedbe7478
Grant <mpls@posteo.net>
2024-06-21 12:42:50 -0500

    update

--- test/replacement_cycling_test.py
+++ test/replacement_cycling_test.py
@@ -16,10 +16,10 @@ base.wait_for_all_edges()
 # Start scenario
 out = base.warcli(f"scenarios run replacement_cycling --network_name={base.network_name}")
 
-log = base.get_tank(0).warnet.container_interface.log
+log_path = base.logfilepath
 
-print("out: {out}")
-print("log: {log}")
+print(f"out: {out}")
+print(f"log: {log_path}")
 
 def check_success():
     False
