b959734c8742c730bf556c164132cb6e9c4f6c3c
Grant <mpls@posteo.net>
2024-06-21 21:03:37 -0500

    update

--- test/test_base.py
+++ test/test_base.py
@@ -88,7 +88,7 @@ class TestBase:
             path = self.tmpdir / "tmp.log"
             if path.exists():
                 with open(path, 'a') as file:
-print(f"PRINT: {line}")
+                    print(f"PRINT: {line}")
                     file.write(line + '\n')
 
         # For kubernetes we assume the server is started outside test base
