9ef199db0e5fd133ea3e72df46433256fe3852fc
Grant <mpls@posteo.net>
2024-06-21 20:45:49 -0500

    updates

--- test/replacement_cycling_test.py
+++ test/replacement_cycling_test.py
@@ -64,18 +64,22 @@ def assert_debug_log(debug_log_path, expected_msgs, unexpected_msgs=None, timeou
 #         print(f"out: {out}")
 
 def log_exists(test_base):
-    path = test_base.tmpdir / "tmp.log"
-    return path.exists()
+    def f():
+        path = test_base.tmpdir / "tmp.log"
+        return path.exists()
+    return f
 
 
 def success_exists(test_base, log_string):
-    path = test_base.tmpdir / "tmp.log"
-    with open(path, 'r') as file:
-        found = False
-        for line in file:
-            if log_string in line:
-                found = True
-                break
+    def f():
+        path = test_base.tmpdir / "tmp.log"
+        with open(path, 'r') as file:
+            found = False
+            for line in file:
+                if log_string in line:
+                    found = True
+                    break
+    return f
 
 
 base = TestBase()
@@ -85,16 +89,17 @@ print(base.warcli(f"network start {graph_file_path}"))
 base.wait_for_all_tanks_status(target="running")
 base.wait_for_all_edges()
 
-
 out = base.warcli(f"scenarios run replacement_cycling --network_name={base.network_name}")
 print(f"Out: {out}")
+
 time.sleep(30)
+
 print("About to wait for log to exist")
 base.wait_for_predicate(log_exists(base))
 print("log exists")
+
 print("about to wait for Finished.")
 base.wait_for_predicate(success_exists(base, "Finished: replacement_cycling.py"))
-
 print(f"Finished: {os.path.basename(__file__)}")
 
 base.stop_server()
